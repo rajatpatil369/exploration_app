a85d4d344d25025e0c9ea804d2ce632158b65248
Rajat Patil <rajatpatil369@gmail.com>
2023-07-06 17:37:10 +0530

    State management in action.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ProviderScope(child: MyApp()));
+
+final taskProvider = StateNotifierProvider<TaskNotifier, List<Task>>((ref) {
+  return TaskNotifier(tasks: [
+    Task(id: 1, label: 'Load rocket with supplies'),
+    Task(id: 2, label: 'Launch rocket'),
+    Task(id: 3, label: 'Circle the home planet'),
+    Task(id: 4, label: 'Head out to the first moon'),
+    Task(id: 5, label: 'Launch moon lander #1')
+  ]);
+});
 
 class MyApp extends StatelessWidget {
   @override
@@ -22,7 +33,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Space Exploration Planner!"),
+        title: Text('Space Exploration Planner!'),
       ),
       body: Column(
         children: [
@@ -34,54 +45,82 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class Progress extends StatelessWidget {
+class Progress extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return const Column(
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(taskProvider);
+    var numTasksCompleted =
+        tasks.where((task) => task.completed == true).length;
+
+    return Column(
       children: [
         Text("You are this far from exploring the whole universe:"),
-        LinearProgressIndicator(value: 0.0),
+        LinearProgressIndicator(value: numTasksCompleted / tasks.length),
       ],
     );
   }
 }
 
-class TaskList extends StatelessWidget {
+class TaskList extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      TaskItem(label: "Load rocket with supplies"),
-      TaskItem(label: "Launch rocket"),
-      TaskItem(label: "Circle the home planet"),
-      TaskItem(label: "Head out to the first moon"),
-      TaskItem(
-        label: "Launch moon lander #1",
-      ),
-    ]);
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(taskProvider);
+
+    return Column(
+      children: tasks.map((task) => TaskItem(task: task)).toList(),
+    );
   }
 }
 
-class TaskItem extends StatefulWidget {
-  final String label;
-  TaskItem({Key? key, required this.label}) : super(key: key);
+class TaskItem extends ConsumerWidget {
+  final Task task;
+  TaskItem({Key? key, required this.task}) : super(key: key);
 
   @override
-  State<TaskItem> createState() => _TaskItemState();
-}
-
-class _TaskItemState extends State<TaskItem> {
-  bool? _value = false;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
         Checkbox(
-          onChanged: (newValue) => setState(() => _value = newValue),
-          value: _value,
+          onChanged: (newValue) =>
+              ref.read(taskProvider.notifier).toggle(task.id),
+          value: task.completed,
         ),
-        Text(widget.label),
+        Text(task.label),
       ],
     );
+  }
+}
+
+@immutable
+class Task {
+  final int id;
+  final String label;
+  final bool completed;
+
+  Task({required this.id, required this.label, this.completed = false});
+
+  Task copyWith({int? id, String? label, bool? completed}) {
+    return Task(
+        id: id ?? this.id,
+        label: label ?? this.label,
+        completed: completed ?? this.completed);
+  }
+}
+
+class TaskNotifier extends StateNotifier<List<Task>> {
+  TaskNotifier({tasks}) : super(tasks);
+
+  void add(Task task) {
+    state = [...state, task];
+  }
+
+  void toggle(int taskId) {
+    state = [
+      for (final item in state)
+        if (taskId == item.id)
+          item.copyWith(completed: !item.completed)
+        else
+          item
+    ];
   }
 }
